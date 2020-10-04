ba431bf65cd9920542fbe2353fbdda08bcac5b3e
jenny ma <jenny_lau_8@hotmail.com>
2020-10-04 16:02:02 -0400

    E2

--- server/routes.js
+++ server/routes.js
@@ -5,16 +5,16 @@ const SEATS_PER_ROW = 12;
 
 // Code that is generating the seats.
 // ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
-    };
-  }
-}
+// const seats = {};
+// const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// for (let r = 0; r < row.length; r++) {
+//   for (let s = 1; s < 13; s++) {
+//     seats[`${row[r]}-${s}`] = {
+//       price: 225,
+//       isBooked: false,
+//     };
+//   }
+// }
 // ----------------------------------
 //////// HELPERS
 const getRowName = (rowIndex) => {
